a96d838b7e915d84574480a3fc331665d36be9b4
bobqiu <xiaoqiu369@gmail.com>
2018-04-27 00:11:22 +0800

    增加zuul,hystrix dashboard+turbine,sleuth+zipkin

--- microservice-consumer-h5-ribbon-hystrix/src/main/java/com/qiu/microserviceconsumerh5ribbonhystrix/MicroserviceConsumerH5RibbonHystrixApplication.java
+++ microservice-consumer-h5-ribbon-hystrix/src/main/java/com/qiu/microserviceconsumerh5ribbonhystrix/MicroserviceConsumerH5RibbonHystrixApplication.java
@@ -1,15 +1,9 @@
 package com.qiu.microserviceconsumerh5ribbonhystrix;
 
-import com.qiu.microserviceconsumerh5ribbonhystrix.conf.MVCConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker //启动断路器支持
 @EnableDiscoveryClient
